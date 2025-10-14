a5d42ba87f0920f11e58ee8e5e05c325628d4884
georges479 <g.ismael071221@gmail.com>
2025-10-14 12:06:25 +0200

    user model

--- BACK_END/models/user.js
+++ BACK_END/models/user.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
+const {isEmail} = require('validator');
 
 // Define the User schema
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email:    { type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true },
-  bio:      { type: String, default: '' },
+  username: { type: String, required: true, unique: true, maxLength: 20, minLength: 3 },
+  email:    { type: String, required: true, unique: true, lowerCase: true, validate: [isEmail], trim: true },
+  passwordHash: { type: String, required: true, trim: true, minLength: 8, maxLength: 1024 },
+  profilePic: { type: String, default: ''},
+  bio:      { type: String, default: '', maxLength: 150 },
   createdAt:{ type: Date, default: Date.now }
 });
 
