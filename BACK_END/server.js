ccd78f43346d8e4ac228210acaf437920773e409
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-10-16 13:33:49 +0200

    Save before update

--- BACK_END/server.js
+++ BACK_END/server.js
@@ -1,10 +1,13 @@
-const http = require('http');
+const express = require('express');
+require('dotenv').config({path: './config/.env'});
 
-const server = http.createServer((req, res) => {
-    // send a test response
-    res.end('Test server');
+// Initialize Express application
+const app = express();
+
+// Create routes for the user
+app.use('/user', require('./routes/userRoutes'));
+
+// App is listening on the specified PORT from environment variables
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
-
-server.listen(5000, () => {
-    console.log('Server is running on port 5000');
-})
