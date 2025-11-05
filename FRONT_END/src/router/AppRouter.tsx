cdd429907c90b639e128a9a89934795792df0044
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-11-05 14:55:29 +0100

    Added navbar to all routes

--- FRONT_END/src/router/AppRouter.tsx
+++ FRONT_END/src/router/AppRouter.tsx
@@ -1,3 +1,4 @@
+import Navbar from "../components/Navbar";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Profile from "../pages/Profile";
@@ -8,6 +9,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const AppRouter = () => {
   return (
     <Router>
+      <Navbar />
         <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
