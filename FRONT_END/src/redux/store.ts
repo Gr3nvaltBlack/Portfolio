eab87e27f98db9ba74fa841b89d6fcc6a90e30c2
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-11-10 11:42:56 +0100

    update store

--- FRONT_END/src/redux/store.ts
+++ FRONT_END/src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from './reducers/rootReducer'
+import { getUsers } from "../actions/get.users.actions";
 
 const store = configureStore({
   reducer: rootReducer,
-  devTools: process.env.NODE_ENV !== "production",
+  devTools: import.meta.env.NODE_ENV !== "production",
 })
 
+store.dispatch(getUsers); // ??
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
