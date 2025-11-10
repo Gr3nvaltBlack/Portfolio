7fa8dd73bc943e8f62e61fa66ba474717d9978a1
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-11-10 12:08:51 +0100

    rooter config

--- FRONT_END/src/redux/reducers/rootReducer.ts
+++ FRONT_END/src/redux/reducers/rootReducer.ts
@@ -1,6 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./user.reducer";
+import usersReducer from "./get.users.reducer";
+import postReducer from "./post.reducer";
 
 export default combineReducers({
     userReducer,
+    usersReducer,
+    postReducer
 });
