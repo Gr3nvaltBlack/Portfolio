4341ee7c76dd1e9a86a01d4ddefc4269704e6cc3
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-11-10 11:33:19 +0100

    adding new properties for the user interface

--- FRONT_END/src/types/user.ts
+++ FRONT_END/src/types/user.ts
@@ -1,5 +1,10 @@
 export interface User {
   _id: string;
   pseudo: string;
+  bio: string;
   picture: string;
+  followers: string[];
+  following: string[];
+  createdAt:string;
+  updatedAt:string;
 }
